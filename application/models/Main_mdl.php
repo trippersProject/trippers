bd79184f4dbfe6af5bf020b450ef362a7041086e
JooHwan Chung <joohwan0607@tong.com>
2024-08-07 16:23:39 +0900

    테이블 컬럼 수정 category->category1

--- application/models/Main_mdl.php
+++ application/models/Main_mdl.php
@@ -22,7 +22,7 @@ class Main_mdl extends CI_Model {
     public function get_article_list($category='', $c_id='') {
         $this->db->select('*');
         $this->db->from('tp_articles');
-        $this->db->join('tp_category', 'tp_category.id = tp_articles.category');
+        $this->db->join('tp_category', 'tp_category.id = tp_articles.category1');
         if($c_id) $this->db->where('c_id', $c_id);
         if($category) $this->db->where('parent', $category);
         $this->db->order_by('sort', 'ASC');
