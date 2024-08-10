b9174eb0c874f144917499d5f936a96b22702036
JooHwan Chung <joohwan0607@tong.com>
2024-08-10 17:01:01 +0900

    메인 html 구조 순서에 맞게 수정 및 추가 백업파일 생성

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ko">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,4 +9,7 @@
   <!--슬라이드/-->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  
+  <link rel="stylesheet" href="assets/css/globals.css" />
+  <link rel="stylesheet" href="assets/css/style.css" />
 </head>
